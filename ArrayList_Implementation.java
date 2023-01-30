56f0cabdf8f49dc89214bcfb6ca0ec09a924591d
utkarsh2603 <utkarsh70888@gmail.com>
2023-01-30 13:20:55 +0530

    Implementation Successful

--- ArrayList_Implementation.java
+++ ArrayList_Implementation.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class ArrayList_Implementation
 {
     public static void main(String args[])
     {
-        System.out.println("Hello");
+        ArrayList<Integer> list=new ArrayList<>();
+        System.out.println("Successfully Implemented");
     }
 }
